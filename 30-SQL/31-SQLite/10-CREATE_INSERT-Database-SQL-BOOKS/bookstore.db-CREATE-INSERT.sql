fa663461f4bec1d5e9dd9478f3f0a4014c9e36ee
allen <developerghost01@gmail.com>
2022-04-11 21:28:09 +0300

    first commit

---
+++ 30-SQL/31-SQLite/10-CREATE_INSERT-Database-SQL-BOOKS/bookstore.db-CREATE-INSERT.sql
@@ -0,0 +1,80 @@
+CREATE TABLE book_types (
+				type_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+				book_type TEXT (50) NOT NULL
+				)
+
+CREATE TABLE authors (
+				author_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+				author_name TEXT (50) NOT NULL
+				)
+
+CREATE TABLE publishers (
+				publisher_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+				publisher_name TEXT (50) NOT NULL,
+				country TEXT (25)
+				)
+
+CREATE TABLE books (
+				book_id INTEGER NOT NULL,
+				book_name TEXT (50) NOT NULL,
+				
+				book_type INTEGER NOT NULL,
+				author_id INTEGER NOT NULL,
+				publisher_id INTEGER NOT NULL,
+				price REAL (5,2),
+				
+				PRIMARY KEY (book_id, book_type, author_id, publisher_id),
+			
+				FOREIGN KEY (book_type) REFERENCES book_types (type_id) 
+					ON DELETE CASCADE ON UPDATE NO ACTION,
+				FOREIGN KEY (author_id) REFERENCES authors (author_id) 
+					ON DELETE CASCADE ON UPDATE NO ACTION,
+				FOREIGN KEY (publisher_id) REFERENCES publishers (publisher_id) 
+					ON DELETE CASCADE ON UPDATE NO ACTION
+				)
+							
+
+INSERT INTO book_types (book_type) VALUES ('Anı')
+INSERT INTO book_types (book_type) VALUES ('Deneme')
+INSERT INTO book_types (book_type) VALUES ('Hikaye')
+INSERT INTO book_types (book_type) VALUES ('Şiir')
+
+INSERT INTO authors (author_name) VALUES ('Ömer Seyfettin')
+INSERT INTO authors (author_name) VALUES ('Halide Edip Adıvar')
+INSERT INTO authors (author_name) VALUES ('Ahmet Rasim')
+INSERT INTO authors (author_name) VALUES ('Necip Fazıl Kısakürek')
+INSERT INTO authors (author_name) VALUES ('Rıfat Ilgaz')
+INSERT INTO authors (author_name) VALUES ('George Orwell')
+INSERT INTO authors (author_name) VALUES ('David Atkinson')
+INSERT INTO authors (author_name) VALUES ('Osman Şahin')
+
+INSERT INTO publishers (publisher_name, country) VALUES ('Can Yayınları', 'TR')
+INSERT INTO publishers (publisher_name, country) VALUES ('Özgür Yayınları', 'TR')
+INSERT INTO publishers (publisher_name, country) VALUES ('Bilge Kültür Sanat', 'TR')
+INSERT INTO publishers (publisher_name, country) VALUES ('Dergah Yayınları', 'TR')
+INSERT INTO publishers (publisher_name, country) VALUES ('Büyük Doğu Yayınları', 'TR')
+INSERT INTO publishers (publisher_name, country) VALUES ('Hacegan Yayınları', 'TR')
+INSERT INTO publishers (publisher_name, country) VALUES ('Çınar Yayınları', 'TR')
+INSERT INTO publishers (publisher_name, country) VALUES ('Amazon', 'US')
+INSERT INTO publishers (publisher_name, country) VALUES ('Joffe Books', 'UK')
+
+INSERT INTO books (book_id , book_name, book_type, author_id, publisher_id, price) 
+VALUES (1, 'Mor Salkımlı Ev', 1, 2, 1, 41.00)
+INSERT INTO books (book_id , book_name, book_type, author_id, publisher_id, price) 
+VALUES (2, 'Vurun Kahpeye', 1, 2, 1, 32.00)
+INSERT INTO books (book_id , book_name, book_type, author_id, publisher_id, price) 
+VALUES (3, 'Vurun Kahpeye', 1, 2, 2, 13.00)
+INSERT INTO books (book_id , book_name, book_type, author_id, publisher_id, price) 
+VALUES (4, 'Şehir Mektupları', 2, 3, 4, 65.00)
+INSERT INTO books (book_id , book_name, book_type, author_id, publisher_id, price) 
+VALUES (5, 'Çile', 4, 4, 5, 100.00)
+INSERT INTO books (book_id , book_name, book_type, author_id, publisher_id, price) 
+VALUES (6, 'Çöle İnen Nur', 3, 4, 5, 120.00)
+INSERT INTO books (book_id , book_name, book_type, author_id, publisher_id, price) 
+VALUES (7, 'Dördüncü Bölük', 3, 5, 7, 21.00)
+INSERT INTO books (book_id , book_name, book_type, author_id, publisher_id, price) 
+VALUES (8, 'Sonuncu İz', 3, 8, 1, 29.00)
+INSERT INTO books (book_id , book_name, book_type, author_id, publisher_id, price) 
+VALUES (9, 'Animal Farm', 3, 6, 8, 55.00)
+INSERT INTO books (book_id , book_name, book_type, author_id, publisher_id, price) 
+VALUES (10, 'Love Byte', 3, 7, 9, 35.00)
